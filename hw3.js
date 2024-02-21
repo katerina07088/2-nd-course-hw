758de39d87be3522bc6eddc8177c77f5bb2857b4
katerina07088 <ekaterinakorotenko@yandex.ru>
2024-02-21 16:01:08 +0300

    changed task5

--- hw3.js
+++ hw3.js
@@ -35,29 +35,17 @@ switch (monthNumber) {
         console.log('Зима');
         break;
     case '3':
-        console.log('Весна');
-        break;
     case '4':
-        console.log('Весна');
-        break;
     case '5':
         console.log('Весна');
         break;
     case '6':
-        console.log('Лето');
-        break;
     case '7':
-        console.log('Лето');
-        break;
     case '8':
         console.log('Лето');
         break;
     case '9':
-        console.log('Осень');
-        break;
     case '10':
-        console.log('Осень');
-        break;
     case '11':
         console.log('Осень');
         break;
